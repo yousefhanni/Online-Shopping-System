8242a2918a0795a3a0e2d56fbcb248f830aa6704
Yousef Hani <91964155+yousefhanni@users.noreply.github.com>
2024-08-20 03:27:19 +0300

    Create Category Model and  Initialize Entity Framework Core with SQL Server Integration
    - Added ApplicationDbContext class to handle the application's database context, with a Categories DbSet to manage category entities.
    - Created the Category model representing product categories in the system, including properties for Id, Name, Description, and CreatedTime.
    - Configured the database connection in appsettings.json using the "DefaultConnection" string to connect to a SQL Server database named 'ShoppingCart'.
    - Updated the Program.cs file to register ApplicationDbContext with the dependency injection container using SQL Server as the database provider.

---
+++ MyShop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Web.Models;
+
+
+namespace Myshop.DAL.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- MyShop.Web/Program.cs
+++ MyShop.Web/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Myshop.DAL.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
